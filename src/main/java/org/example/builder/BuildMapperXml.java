fdebd6e2751b6d0fcb26bb8f6d4fe7fe58f859ba
FuYongquan <1304184660@qq.com>
2025-04-13 05:29:57 +0800

    day6-生成MapperXml_创建根据主更新逻辑

--- src/main/java/org/example/builder/BuildMapperXml.java
+++ src/main/java/org/example/builder/BuildMapperXml.java
@@ -508,9 +508,37 @@ public class BuildMapperXml {
                 bw.newLine();
 
                 //添加注释
+                bw.write("\t<!-- \"根据\"" + commentParams + "\"更新\" -->");
+                bw.newLine();
+                bw.write("\t<update id=\"updateBy" + methodName + "\" parameterType=\"" + poClass + "\">");
+                bw.newLine();
+                bw.write("\t\tUPDATE " + tableInfo.getTableName());
+                bw.newLine();
+                bw.write("\t\t<set>");
+                bw.newLine();
+                for( FieldInfo fieldInfo : tableInfo.getFieldList()){
+                    if(fieldInfo.getIsAutoIncrement()){
+                        continue;
+                    }
+                    bw.write("\t\t\t<if test=\"bean." + fieldInfo.getPropertyName() + " != null\">");
+                    bw.newLine();
+                    bw.write("\t\t\t\t" + fieldInfo.getFieldName() + " = #{bean." + fieldInfo.getPropertyName() + "},");
+                    bw.newLine();
+                    bw.write("\t\t\t</if>");
+                    bw.newLine();
+                }
+                bw.write("\t\t</set>");
+                bw.newLine();
+                bw.write("\t\twhere " + params);
+                bw.newLine();
+                bw.write("\t</update>");
+                bw.newLine();
+                bw.newLine();
+
+                //添加注释
                 bw.write("\t<!-- \"根据\"" + commentParams + "\"删除\" -->");
                 bw.newLine();
-                bw.write("\t<delete id=\"selectBy" + methodName + "\">");
+                bw.write("\t<delete id=\"deleteBy" + methodName + "\">");
                 bw.newLine();
                 bw.write("\t\tDELETE FROM " + tableInfo.getTableName() + " where " + params);
                 bw.newLine();
